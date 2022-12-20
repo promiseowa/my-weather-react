ee339af96b8d1c6543d28e164d36a5e26297874f
promise <108261992+promiseowa@users.noreply.github.com>
2022-12-20 21:14:11 +0100

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import Weather from "./weather";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
